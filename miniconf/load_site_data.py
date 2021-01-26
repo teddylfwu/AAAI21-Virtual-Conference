38089b3ee9f3860e7f9a04007887a7dbfc77420e
Chen <ronalovmj@gmail.com>
2021-01-26 10:38:10 -0500

    Update load_site_data.py

--- miniconf/load_site_data.py
+++ miniconf/load_site_data.py
@@ -541,7 +541,7 @@ def generate_tutorial_events(site_data: Dict[str, Any]):
                 max_end = max([t["end_time"] for t in block])
                 if uid == "UC":
                     event = {
-                        "title": f"<b>AAAI-21 Tutorial Forum</b><br/><b>{uid}: {tutorial['title']}</b><br/><i>{tutorial['organizers']}</i>",
+                        "title": f"{tutorial['title']}</b><br/><i>{tutorial['organizers']}</i>",
                         "start": min_start,
                         "end": max_end,
                         "location": f"undergraduate_consortium.html",
@@ -552,7 +552,7 @@ def generate_tutorial_events(site_data: Dict[str, Any]):
                     }
                 else:
                     event = {
-                        "title": f"<b>AAAI-21 Tutorial Forum</b><br/><b>{uid}: {tutorial['title']}</b><br/><i>{tutorial['organizers']}</i>",
+                        "title": f"<b>Undergraduate Consortium</b><br/><b>{uid}: {tutorial['title']}</b><br/><i>{tutorial['organizers']}</i>",
                         "start": min_start,
                         "end": max_end,
                         "location": f"paper_{uid}.html",
