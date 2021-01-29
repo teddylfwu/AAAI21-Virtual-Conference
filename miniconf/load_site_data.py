386083eccc332a4a919d3c6f5c0de46be700478d
Chen <ronalovmj@gmail.com>
2021-01-28 21:25:28 -0500

    Add New Faculty Highlights Program

--- miniconf/load_site_data.py
+++ miniconf/load_site_data.py
@@ -247,6 +247,7 @@ def load_site_data(
     # undergraduate_consortium.html
     tutorial_UC = []
     tutorial_OTHER = []
+    tutorial_FH = []
 
     for item in site_data["tutorials"]:
         if "MQ" in item["UID"]:
@@ -261,6 +262,8 @@ def load_site_data(
             tutorial_OTHER.append(item)
         if "UC" in item["UID"]:
             tutorial_UC.append(item)
+        if "FH" in item["UID"]:
+            tutorial_FH.append(item)
 
     tutorials = build_tutorials(site_data["tutorials"])
 
@@ -273,6 +276,7 @@ def load_site_data(
     site_data["tutorials_AQ"] = build_tutorials(tutorial_AQ)
     site_data["tutorials_AH"] = build_tutorials(tutorial_AH)
     site_data["tutorials_UC"] = build_tutorials(tutorial_UC)
+    site_data["tutorials_FH"] = build_tutorials(tutorial_FH)
     site_data["tutorials_OTHER"] = build_tutorials(tutorial_OTHER)
     # tutorial_<uid>.html
     by_uid["tutorials"] = {tutorial.id: tutorial for tutorial in tutorials}
